2c9ac173510860452f3ad64d0af00852227061ce
Jesse <dynaptx@163.com>
2020-05-16 15:27:10 +0800

    新增封装DOM

---
+++ demos-Preview/封装DOM/main.js
@@ -0,0 +1,22 @@
+window.dom = {
+    find(selector) {
+        return document.querySelectorAll(selector)
+    },
+
+    style(node, name, value) {
+        node.style[name] = value
+    },
+
+    each(nodeList, fn) {
+        for (let i = 0; i < nodeList.length; i++) {
+            fn.call(null, nodeList[i])
+        }
+    }
+}
+
+
+const div = dom.find('#test>.red')[0] // 获取对应的元素
+dom.style(div, 'color', 'red') // 设置 div.style.color
+
+const divList = dom.find('.red') // 获取多个 div.red 元素
+dom.each(divList, (n) => console.log(n)) // 遍历 divList 里的所有元素
